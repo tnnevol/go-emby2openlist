3b16b1fcc8735e5f264f27c3697a7f19ccd2c536
ambitious <1535820771@qq.com>
2025-07-05 15:37:18 +0800

    perf: expose WrapColor func

--- internal/util/colors/colors.go
+++ internal/util/colors/colors.go
@@ -1,13 +1,15 @@
 package colors
 
+type C string
+
 // 日志颜色输出常量
 const (
-	Blue   = "\x1b[38;2;090;156;248m"
-	Green  = "\x1b[38;2;126;192;080m"
-	Yellow = "\x1b[38;2;220;165;080m"
-	Red    = "\x1b[38;2;228;116;112m"
-	Purple = "\x1b[38;2;160;186;250m"
-	Gray   = "\x1b[38;2;145;147;152m"
+	Blue   C = "\x1b[38;2;090;156;248m"
+	Green  C = "\x1b[38;2;126;192;080m"
+	Yellow C = "\x1b[38;2;220;165;080m"
+	Red    C = "\x1b[38;2;228;116;112m"
+	Purple C = "\x1b[38;2;160;186;250m"
+	Gray   C = "\x1b[38;2;145;147;152m"
 
 	reset = "\x1b[0m"
 )
@@ -26,40 +28,40 @@ func SetEnabler(e Enabler) { enabler = e }
 
 // ToBlue 将字符串转成蓝色
 func ToBlue(str string) string {
-	return wrapColor(Blue, str)
+	return WrapColor(Blue, str)
 }
 
 // ToGreen 将字符串转成绿色
 func ToGreen(str string) string {
-	return wrapColor(Green, str)
+	return WrapColor(Green, str)
 }
 
 // ToYellow 将字符串转成黄色
 func ToYellow(str string) string {
-	return wrapColor(Yellow, str)
+	return WrapColor(Yellow, str)
 }
 
 // ToRed 将字符串转成红色
 func ToRed(str string) string {
-	return wrapColor(Red, str)
+	return WrapColor(Red, str)
 }
 
 // ToPurple 将字符串转成紫色
 func ToPurple(str string) string {
-	return wrapColor(Purple, str)
+	return WrapColor(Purple, str)
 }
 
 // ToGray 将字符串转成灰色
 func ToGray(str string) string {
-	return wrapColor(Gray, str)
+	return WrapColor(Gray, str)
 }
 
-// wrapColor 将字符串 str 包裹上指定颜色的 ANSI 字符
+// WrapColor 将字符串 str 包裹上指定颜色的 ANSI 字符
 //
 // 如果用户关闭了颜色输出, 则直接返回原字符串
-func wrapColor(color, str string) string {
+func WrapColor(color C, str string) string {
 	if enabler != nil && !enabler.EnableColor() {
 		return str
 	}
-	return color + str + reset
+	return string(color) + str + reset
 }
